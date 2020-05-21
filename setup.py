dbe9be559a75caa799ca03f4627583ab995f4d8c
Alex <alex.thiel@aotu.ai>
2020-05-21 09:50:43 -0700

    Added long_description to the setup.py, which will show up on the pypi page.

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
     name="brainframe-api",
     version="0.26.0",
     description="Provides a Python wrapper around the BrainFrame REST API.",
+    long_description=open("README.rst").read(),
     author="Aotu",
     packages=find_namespace_packages(
         include=["brainframe.api*"]
     ),
-
     install_requires=[
         "requests==2.*",
         "pillow==6.*",
