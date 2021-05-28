7dd06777e7f6e249d8c77a80c2c3218c8f20964c
Tyler Compton <tyler.compton@aotu.ai>
2021-05-28 11:59:40 -0700

    Take from_dict out of try
    Co-authored-by: ignormies <bryce.beagle@aotu.ai>

--- brainframe/api/stubs/cloud_users.py
+++ brainframe/api/stubs/cloud_users.py
@@ -17,6 +17,7 @@ class CloudUsersStubMixIn(BaseStub):
         req = f"/api/cloud_user"
         try:
             data, _ = self._get_json(req, timeout=timeout)
-            return CloudUserInfo.from_dict(data)
         except CloudUserNotFoundError:
             return None
+        else:
+            return CloudUserInfo.from_dict(data)
