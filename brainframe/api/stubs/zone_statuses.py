d32a7a92378481868eb280355745d0f494d87a94
Stephen Li <stephen@aotu.ai>
2025-01-04 11:48:47 -0800

    Revert "Clean up/revert the change introduced by a19a88"
    This reverts commit bb9725675ae31c60881a894ad5cd5003ac277cad.

--- brainframe/api/stubs/zone_statuses.py
+++ brainframe/api/stubs/zone_statuses.py
@@ -1,4 +1,5 @@
 import json
+import time
 from typing import Dict, Generator
 
 import requests
@@ -51,6 +52,7 @@ class ZoneStatusStubMixin(BaseStub):
             resp = self._get(req, timeout=timeout)
 
             packets = resp.iter_lines(delimiter=b"\r\n")
+            timeout_start = time.time()
             while True:
                 try:
                     packet = next(packets)
