6e9084f5cbcf7e49aae3f4a62fc5467211b34507
Stephen Li <stephen@aotu.ai>
2025-01-04 11:38:38 -0800

    Revert "Made the get_zone_status_stream api return an empty dict when to keep the long connection alive when there is no analyzing results"
    This reverts commit 0146c4f3aac0c6447b15b40d2e27bfbe5f35a3da.

--- brainframe/api/stubs/zone_statuses.py
+++ brainframe/api/stubs/zone_statuses.py
@@ -66,15 +66,18 @@ class ZoneStatusStubMixin(BaseStub):
                     raise bf_errors.ServerNotReadyError(message)
 
                 if packet == b'':
-                    processed={}
-                else:
-                    # Parse the line
-                    zone_statuses_dict = json.loads(packet)
+                    if time.time() < timeout_start + timeout:
+                        continue
+                    else:
+                        break
 
-                    processed = {
-                        int(s_id): {key: bf_codecs.ZoneStatus.from_dict(val)
-                                    for key, val in statuses.items()}
-                        for s_id, statuses in zone_statuses_dict.items()}
+                # Parse the line
+                zone_statuses_dict = json.loads(packet)
+
+                processed = {
+                    int(s_id): {key: bf_codecs.ZoneStatus.from_dict(val)
+                                for key, val in statuses.items()}
+                    for s_id, statuses in zone_statuses_dict.items()}
                 yield processed
 
         return zone_status_iterator()
